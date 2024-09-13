d4dceb54c3c3ba9ec0dbc9e7cc42dc3f4cbcda2b
TuiLaZit <147925219+TuiLaZit@users.noreply.github.com>
2024-09-13 11:28:09 +0700

    Update home.php

--- pages/main/home.php
+++ pages/main/home.php
@@ -1,3 +1,18 @@
-<div class="home_container">
-<p>k</p>
+<div id="home_container">
+    <div class="home__button__tray tray" id="home__main-tray">
+        <div class="main-tray__button button" id="kit-button">
+            <h1>Áo Đấu</h1>
+        </div>
+        <div class="main-tray__button button" id="retro-button">
+            <h1>Áo Retro</h1>
+        </div>
+    </div>
+    <div class="home__button__tray tray" id="home__sub-tray">
+        <div class="sub-tray__button button" id="etc-button">
+            <h1>Áo Khác</h1>
+        </div>
+        <div class="sub-tray__button button" id="acs-button">
+            <h1>Phụ Kiện</h1>
+        </div>
+    </div>
 </div>
