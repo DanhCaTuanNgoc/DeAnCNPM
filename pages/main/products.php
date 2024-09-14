9ef059ebb6791a2ea9bc80aab7de51efc9fc05e5
Nguyễn Đại Quốc <127086245+NguyenDaiQuoc@users.noreply.github.com>
2024-09-14 20:54:36 +0700

    Update products.php

--- pages/main/products.php
+++ pages/main/products.php
@@ -1,1 +1,14 @@
-<div><p>products</p></div>
+<div class="product_container" >
+        <div class="Product_img">
+            <img src="D://PHP/htdocs/DeAnCNPM-main/images/slidepic1.jpg" alt="Áo khoác" class="L_img">
+            <div>
+                <img src="D://PHP/htdocs/DeAnCNPM-main/images/slidepic2.jpg" alt="Mặt trước" class="S_img">
+                <img src="D://PHP/htdocs/DeAnCNPM-main/images/slidepic3.jpg" alt="Mặt sau" class="S_img">
+                <img src="D://PHP/htdocs/DeAnCNPM-main/images/slidepic4.jpg" alt="Mặt trái" class="S_img">
+                <img src="D://PHP/htdocs/DeAnCNPM-main/images/slidepic5.jpg" alt="Mặt phải" class="S_img">
+            </div>
+        </div>
+        <div class="Product_info">
+            <p>abcd</p>
+        </div>
+</div>
