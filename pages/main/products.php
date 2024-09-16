c781ddff48d801fb9128fb699c2183f9b976f45a
Nguyễn Đại Quốc <127086245+NguyenDaiQuoc@users.noreply.github.com>
2024-09-16 18:38:01 +0700

    Update products.php

--- pages/main/products.php
+++ pages/main/products.php
@@ -1,14 +1,173 @@
-<div class="product_container" >
-        <div class="Product_img">
-            <img src="D://PHP/htdocs/DeAnCNPM-main/images/slidepic1.jpg" alt="Áo khoác" class="L_img">
+    <div class="product_container" style="flex: auto;">
+        <div class="product_img">
+            <img id="l_img" src="D://PHP/htdocs/DeAnCNPM-main/images/slidepic1.jpg" alt="Áo khoác" class="L_img">
             <div>
+                <!-- <div class="edit_simg"> -->
+                <button class="btn_left" style="font-size: 18px;"><strong>
+                        << /strong></button>
                 <img src="D://PHP/htdocs/DeAnCNPM-main/images/slidepic2.jpg" alt="Mặt trước" class="S_img">
                 <img src="D://PHP/htdocs/DeAnCNPM-main/images/slidepic3.jpg" alt="Mặt sau" class="S_img">
                 <img src="D://PHP/htdocs/DeAnCNPM-main/images/slidepic4.jpg" alt="Mặt trái" class="S_img">
                 <img src="D://PHP/htdocs/DeAnCNPM-main/images/slidepic5.jpg" alt="Mặt phải" class="S_img">
+                <button class="btn_right" style="font-size: 18px;"><strong>></strong></button>
             </div>
         </div>
         <div class="Product_info">
-            <p>abcd</p>
+            <h2>
+                <strong>Áo khoác Arsenal 2023 đen sọc vàng logo súng vải gió trượt nước, chống nắng</strong>
+            </h2>
+            <div class="inventory_quantity">
+                <span>
+                    <strong>Tình trạng:</strong>
+                </span>
+                <span>
+                    <span style="color: red;">Còn hàng</span>
+                </span>
+            </div>
+            <div class="price">
+                <span><strong>390,000₫</strong></span>
+            </div>
+            <div class="msgbtn">
+                <img src="D://PHP/htdocs/DeAnCNPM-main/images/msgbtn.jfif" alt="msgbtn" class="msgimg">
+                <a href="m.me/7tcc" target="_blank" style="color: rgb(33, 206, 206);">Nhắn tin cho chúng tôi</a>
+            </div>
+            <div class="size">
+                <label style="font-size: 18px;"><strong>Kích thước:</strong></label>
+                <select name="size" style="font-size: 18px;">
+                    <option value="S">S</option>
+                    <option value="M">M</option>
+                    <option value="L">L</option>
+                    <option value="XL">XL</option>
+                    <option value="XXL">XXL</option>
+                </select>
+            </div>
+            <div class="amount">
+                <div>
+                    <label style="font-size: 18px;"></label>
+                    <strong>Số lượng</strong>
+                    </label>
+                </div>
+                <div class="amount_btn">
+                    <button class="minus_btn" style="font-size: 20px;"><strong>-</strong></button>
+                    </button>
+                    <input type="text" name="" id="amount" class="amount_input" style="font-size: 20px;" size="1"
+                        value="1" max="20">
+                    <button class="plus_btn" style="font-size: 20px;"><strong>+</strong></button>
+                </div>
+            </div>
+            <div class="buybtn_hotlinebtn">
+                <button class="buy_btn">
+                    <p style="font-size: 18px;"><strong>MUA NGAY</strong></p>
+                    <span>Giao hàng tận nơi</span>
+                </button>
+                <div class="hotline_btn">
+                    <p style="font-size: 18px;"><strong>Hãy gọi</strong></p>
+                    <p>Liên hệ: 0938688079</p>
+                </div>
+            </div>
         </div>
-</div>
+
+    </div>
+    <div class="info_container">
+        <div class="header">
+            <div class="info">
+                <button class="info_btn">
+                    <span>THÔNG TIN SẢN PHẨM</span>
+                </button>
+            </div>
+            <div class="stat">
+                <button class="stat_btn">
+                    <span>THÔNG SỐ SẢN PHẨM</span>
+                </button>
+
+            </div>
+        </div>
+        <div class="body">
+            <p>
+            <h2>Áo khoác Arsenal 2023 đen sọc vàng logo súng vải gió trượt nước, chống nắng</h2>
+            </p>
+            <p>
+                Sản phẩm này được bảo hành logo trọn đời miễn phí tại Nhện Shop, chính sách bảo hành tốt nhất trong tất
+                cả các cửa hàng thể thao trên cả nước.
+            </p>
+            <p>
+                - Kiểu dáng : phom mới nhất năm nay, áo rất nhiều tính năng có thể kể đến : vải trượt nước đi mưa nhẹ,
+                chống gió lùa, chống lạnh, nón tháo rời được, áo được may 2 lớp có túi trong, túi ngoài có khóa kéo.
+            </p>
+            <p style="text-align: center;">
+                <img src="D://PHP/htdocs/DeAnCNPM-main/images/size_body.webp" alt="size_body" class="size_body">
+            </p>
+            <p>
+                - Sản phẩm này được bảo hành logo trọn đời miễn phí tại <strong>@nhện Shop</strong>, chính sách bảo hành
+                tốt nhất trong tất cả các cửa hàng thể thao trên cả nước
+            </p>
+            <p>
+                Các bạn xem thêm một số hình ảnh khách của shop mặc áo này nhé :
+            </p>
+            <p>
+                - Anh em có nhu cầu trang trí thêm mặt lưng áo xin vui lòng liên hệ<strong>facebook Nhện
+                    Shop</strong>(https://www.facebook.com/nhen.vn) hoặc facebook <strong>Nguyễn Thùy Trâm (nhện shop)
+                    https://www.facebook.com/thuytrams</strong>
+            </p>
+        </div>
+        <div class="related_product">
+            <div>
+                <h2>
+                    <a target="_blank" style="color: black;text-decoration: none;"
+                        href="www.7tcc.vn/collections"><strong>Có thể bạn cũng thích?</strong></a>
+                </h2>
+            </div>
+            <div class="product_showmore">
+                <div class="product_box">
+                    <div>
+                        <a target="_blank" href="https://7tcc.vn/aodau1"></a>
+                        <img src="D://PHP/htdocs/DeAnCNPM-main/images/slidepic1.jpg" alt="Áo đấu 1" class="M_img">
+                        </a>
+                    </div>
+                    <div>
+                        <a target="_blank" href="https://7tcc.vn/aodau1" style="text-decoration: none;">
+                            <p style="color: black;">Áo đấu 1</p>
+                        </a>
+                        <div class="price_box">
+                            <span><strong>390,000₫</strong></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="product_box">
+                    <div>
+                        <a target="_blank" href="https://7tcc.vn/aodau2"></a>
+                        <img src="D://PHP/htdocs/DeAnCNPM-main/images/slidepic1.jpg" alt="Áo đấu 2" class="M_img">
+                        </a>
+                    </div>
+                    <div>
+                        <a target="_blank" href="https://7tcc.vn/aodau2" style="text-decoration: none;">
+                            <p style="color: black;">Áo đấu 2</p>
+                        </a>
+                        <div class="price_box">
+                            <span><strong>390,000₫</strong></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="product_box">
+                    <div>
+                        <a target="_blank" href="https://7tcc.vn/aodau3"></a>
+                        <img src="D://PHP/htdocs/DeAnCNPM-main/images/slidepic1.jpg" alt="Áo đấu 3" class="M_img">
+                        </a>
+                    </div>
+                    <div>
+                        <a target="_blank" href="https://7tcc.vn/aodau1" style="text-decoration: none;">
+                            <p style="color: black;">Áo đấu 3</p>
+                        </a>
+                        <div class="price_box">
+                            <span><strong>390,000₫</strong></span>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+    </div>
+    </div>
