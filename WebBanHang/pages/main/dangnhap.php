419fd5914731f80ed504b44db56750c425fadfec
DanhCaTuanNgoc <127586212+DanhCaTuanNgoc@users.noreply.github.com>
2024-09-24 21:13:17 +0700

    Add files via upload

--- WebBanHang/pages/main/dangnhap.php
+++ WebBanHang/pages/main/dangnhap.php
@@ -1,3 +1,24 @@
 <div class="main_content">
-    <p>đăng nhập</p>
+    <div class="login_container">
+        <p style="font-weight: bold; font-size : 20px; margin : 20px 0 10px 0;">Đăng nhập</p>
+        <form class="login_content">
+            <!-- CHUA LAM VANLIDATE EMAIL -->
+            <div class="input-box">
+                <span class="icon"><ion-icon name="mail"></ion-icon></span>
+                <input type="text" class="email" required>
+                <label>Email:</label>
+            </div>
+            <div class="input-box">
+                <!-- <span class="icon"><ion-icon name="lock-closed"></ion-icon></span> -->
+                <input type="password" class="password" required>
+                <label>Mật khẩu:</label>
+            </div>
+            <div class="remember-forgot">
+                <label><input type="checkbox">Lưu đăng nhập</label>
+                <a href="#">Quên mật khẩu?</a>
+            </div>
+            <button class="login_form_btn" type="submit">Đăng nhập</button>
+            <p>Chưa có tài khoản?<a class="registerlink" href="#">Đăng ký</a></p>
+        </form>
+    </div>
 </div>
