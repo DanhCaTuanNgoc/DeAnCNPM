978e37ab13f45c5ac93cd330fc60f11cc26747f6
BurnHocCodee <luongthanhtuan525@gmail.com>
2024-09-27 15:00:55 +0700

    Create timKiem.php

---
+++ Web_ban_hang_php/pages/main/timKiem.php
@@ -0,0 +1,25 @@
+<?php
+	if(isset($_GET['tuKhoa'])){
+		$tuKhoa = $_GET['tuKhoa'];
+	}else{
+		$tuKhoa = '';
+	}
+    $sql_pro= "SELECT * FROM tbl_sanpham WHERE ten_sp LIKE '%".$tuKhoa."%'";
+    $query_pro= mysqli_query($mysqli,$sql_pro);
+?>
+<h3>Sản Phẩm Mới Nhất</h3>
+<ul class="product_list">
+	<?php 
+	while($row = mysqli_fetch_array($query_pro)){
+	?>
+	<li>
+		<a href="index.php?quanly=sanPham&id=<?php echo $row['id_sp']?>">
+		<img src="admincp/modules/quanLySanPham/uploads/<?php echo $row['hinh_anh'] ?>">
+		<p class="product_name"><?php echo $row['ten_sp'] ?></p>
+		<p class="product_price"><?php echo number_format($row['gia_sp'],0,',','.').' VND ' ?></p>
+		</a>
+						
+	</li>
+	<?php 
+	} ?>
+</ul>
