e808bcd58ba649a103ad1c8d0f288cad73ce1dd4
BurnHocCodee <luongthanhtuan525@gmail.com>
2024-09-28 12:58:01 +0700

    Update menu.php

--- Web_ban_hang_php/admincp/modules/menu.php
+++ Web_ban_hang_php/admincp/modules/menu.php
@@ -4,4 +4,5 @@
 	<li><a href="index.php?action=quanLySanPham&query=them">Quản lý sản phẩm</a></li>
 	<li><a href="index.php?action=quanLyGioHang&query=them">Quản lý giỏ hàng</a></li>
 	<li><a href="index.php?action=quanLyTinTuc&query=them">Quản lý tin tức</a></li>
+	<li><a href="index.php?action=quanLyTinTuc&query=lietKe">Quản lý don hang</a></li>
 </ul>
