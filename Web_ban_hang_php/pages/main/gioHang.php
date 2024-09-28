ab02f006a2bcee974f05a4b4859a747e79c0f836
BurnHocCodee <luongthanhtuan525@gmail.com>
2024-09-28 12:58:22 +0700

    Update gioHang.php

--- Web_ban_hang_php/pages/main/gioHang.php
+++ Web_ban_hang_php/pages/main/gioHang.php
@@ -1,12 +1,12 @@
 
-<h3>Gio hang
+<p>Gio hang
   <br>
   <?php 
   if(isset($_SESSION['dang_ky'])){
     echo 'Xin Chào: '.'<span style="color : red">'.$_SESSION['dang_ky'].'</span>';
   }
   ?>
-</h3>
+</p>
   <table style="width: 100%; text-align: center; border-collapse: collapse;" border="1">
   <tr>
     <th>Id</th>
@@ -20,12 +20,12 @@
   </tr>
   <?php
   if(isset($_SESSION['cart'])){
-  	$i=0;
-  	$tongtien=0;
-  	foreach($_SESSION['cart'] as $cart_item){
-  		$thanhtien= $cart_item['so_luong']*$cart_item['gia_sp'];
-  		$tongtien+=$thanhtien;
-  		$i++;
+    $i=0;
+    $tongtien=0;
+    foreach($_SESSION['cart'] as $cart_item){
+      $thanhtien= $cart_item['so_luong']*$cart_item['gia_sp'];
+      $tongtien+=$thanhtien;
+      $i++;
   ?>
   <tr>
     <td><?php echo $i; ?></td>
@@ -33,26 +33,26 @@
     <td><?php echo $cart_item['ten_sp']  ?></td>
     <td><img src="admincp/modules/quanLySanPham/uploads/<?php echo $cart_item['hinh_anh'];?>"width="150px"></td>
     <td>
-    	<a href="pages/main/themGioHang.php?cong=<?php echo $cart_item['id']?>"><i class="fa-solid fa-plus"></i></a>
-    	<?php echo $cart_item['so_luong'] ?>
-    	<a href="pages/main/themGioHang.php?tru=<?php echo $cart_item['id']?>"><i class="fa-solid fa-minus"></i></a>	
-    	</td>
+      <a href="pages/main/themGioHang.php?cong=<?php echo $cart_item['id']?>"><i class="fa-solid fa-plus"></i></a>
+      <?php echo $cart_item['so_luong'] ?>
+      <a href="pages/main/themGioHang.php?tru=<?php echo $cart_item['id']?>"><i class="fa-solid fa-minus"></i></a>  
+      </td>
     <td><?php echo number_format($cart_item['gia_sp'],0,',','.').' VND ' ?></td>
     <td><?php echo number_format($thanhtien,0,',','.').' VND ' ?></td>
     <td><a href="pages/main/themGioHang.php?xoa=<?php echo $cart_item['id']?>">Xoa</a></td>
   </tr>
-	<?php 
-	}
-	?>
-	<tr>
-		<td colspan="8">
-			<p style="float: left;">Tong tien : <?php echo number_format($tongtien,0,',','.').' VND ' ?></p>
-			<p style="float: right;"><a href="pages/main/themGioHang.php?xoatatca=1">Xoa tat ca</a></p>
+  <?php 
+  }
+  ?>
+  <tr>
+    <td colspan="8">
+      <p style="float: left;">Tong tien : <?php echo number_format($tongtien,0,',','.').' VND ' ?></p>
+      <p style="float: right;"><a href="pages/main/themGioHang.php?xoatatca=1">Xoa tat ca</a></p>
       <div style="clear: both;"></div>
       <?php  
       if(isset($_SESSION['dang_ky'])){
       ?>
-        <p><a href="index.php?quanly=thanhToan">Đặt Hàng</a></p>
+        <p><a href="pages/main/thanhToan.php">Đặt Hàng</a></p>
       <?php 
       }else{
       ?>
@@ -60,14 +60,14 @@
       <?php 
       }
       ?>
-		</td>
-	</tr>
-	<?php 
-	}else{
-	?>
-	<tr>
-		<td colspan="8"><p>Hien tai khong co san pham</p></td>
-	</tr>
-	<?php 
-	} ?>
+    </td>
+  </tr>
+  <?php 
+  }else{
+  ?>
+  <tr>
+    <td colspan="8"><p>Hien tai khong co san pham</p></td>
+  </tr>
+  <?php 
+  } ?>
 </table> 
