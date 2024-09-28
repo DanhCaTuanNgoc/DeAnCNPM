378d53348c3afc78140f4579bb4b7845afe2a911
BurnHocCodee <luongthanhtuan525@gmail.com>
2024-09-28 14:16:03 +0700

    Update index.php

--- Web_ban_hang_php/pages/main/index.php
+++ Web_ban_hang_php/pages/main/index.php
@@ -1,5 +1,15 @@
 <?php
-    $sql_pro= "SELECT * FROM tbl_sanpham,tbl_danhmucqa WHERE tbl_sanpham.id_dm=tbl_danhmucqa.id_dm ORDER BY tbl_sanpham.id_sp DESC LIMIT 25";
+	if(isset($_GET['trang'])){
+		$page = $_GET['trang'];
+	}else{
+		$page='';
+	}
+	if($page=='' || $page==1){
+		$begin = 0;
+	}else{
+		$begin = ($page*5)-5;
+	}
+    $sql_pro= "SELECT * FROM tbl_sanpham,tbl_danhmucqa WHERE tbl_sanpham.id_dm=tbl_danhmucqa.id_dm ORDER BY tbl_sanpham.id_sp DESC LIMIT $begin,5";
     $query_pro= mysqli_query($mysqli,$sql_pro);
 ?>
 <h3>Sản Phẩm Mới Nhất</h3>
@@ -17,4 +27,20 @@
 	</li>
 	<?php 
 	} ?>
+</ul>
+<div style="clear:both;"></div>
+<?php 
+	$sql_page = mysqli_query($mysqli,"SELECT * FROM tbl_sanpham");
+	$row_count = mysqli_num_rows($sql_page);
+	$trang =ceil($row_count/3);
+?>
+<p>Trang hien tai: <?php echo $page ?></p>
+<ul class="list_page">
+	<?php 
+	for ($i=1; $i < $trang ; $i++) {
+	?>
+	<li <?php if($i==$page){echo 'style="background: #e69833;"';}else{echo '';} ?>><a href="index.php?trang=<?php echo $i ?>"><?php echo $i ?></a></li>
+	<?php 
+	}
+	?>
 </ul>
