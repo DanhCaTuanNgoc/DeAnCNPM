3ba13ceb25281294f0bfb4c1ee6100befb4c2401
BurnHocCodee <luongthanhtuan525@gmail.com>
2024-09-29 17:17:44 +0700

    Update thanhToan.php

--- Web_ban_hang_php/pages/main/thanhToan.php
+++ Web_ban_hang_php/pages/main/thanhToan.php
@@ -1,20 +1,20 @@
 <?php 
   session_start();
   include('../../admincp/config/config.php');
-  	$id_khachhang = $_SESSION['id_khachhang'];
-  	$ma_gh = rand(0,9999);
-  	$insert_cart = "INSERT INTO tbl_giohang(id_khachhang,ma_gh,trang_thai) VALUE('".$id_khachhang."','".$ma_gh."',1)";
-  		$cart_query = mysqli_query($mysqli,$insert_cart);
-  		if($cart_query){
-  			// add gio hang chi tiet
-  			foreach($_SESSION['cart'] as $key => $value){
-  				$id_sp = $value['id'];
-  				$so_luong = $value['so_luong'];
-  				$insert_order_details= "INSERT INTO tbl_chitiet_gh(ma_gh,id_sp,so_luong) VALUE('".$ma_gh."','".$id_sp."','".$so_luong."')";
-  				mysqli_query($mysqli,$insert_order_details);
-  			}
-  		}
-  		unset($_SESSION['cart']);
-  		header('Location:../../index.php?quanly=camMon');
+    $id_khachhang = $_SESSION['id_khachhang'];
+    $ma_gh = rand(0,9999);
+    $insert_cart = "INSERT INTO tbl_giohang(id_khachhang,ma_gh,trang_thai) VALUE('".$id_khachhang."','".$ma_gh."',1)";
+      $cart_query = mysqli_query($mysqli,$insert_cart);
+      if($cart_query){
+        // add gio hang chi tiet
+        foreach($_SESSION['cart'] as $key => $value){
+          $id_sp = $value['id'];
+          $so_luong = $value['so_luong'];
+          $insert_order_details= "INSERT INTO tbl_chitiet_gh(ma_gh,id_sp,so_luong_mua) VALUE('".$ma_gh."','".$id_sp."','".$so_luong."')";
+          mysqli_query($mysqli,$insert_order_details);
+        }
+      }
+      unset($_SESSION['cart']);
+      header('Location:../../index.php?quanly=camMon');
 
 ?>
