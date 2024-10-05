fbfc06023a0fb5d58fc3ef825bddf6b6a4a11d28
Nguyễn Đại Quốc <127086245+NguyenDaiQuoc@users.noreply.github.com>
2024-10-05 09:20:46 +0700

    Update danhMucBaiViet.php

--- Web_ban_hang_php/pages/main/danhMucBaiViet.php
+++ Web_ban_hang_php/pages/main/danhMucBaiViet.php
@@ -1,21 +1,21 @@
 <?php
-    $sql_bv= "SELECT * FROM tbl_bv WHERE tbl_bv.id_bv='$_GET[id]' ORDER BY id_bv DESC";
+    $sql_bv= "SELECT * FROM tbl_baiviet WHERE tbl_baiviet.id='$_GET[id]' ORDER BY id DESC";
     $query_bv= mysqli_query($mysqli,$sql_bv);
-    $sql_name= "SELECT * FROM tbl_danhmucbv WHERE tbl_danhmucbv.id_bv='$_GET[id]' LIMIT 1";
+    $sql_name= "SELECT * FROM tbl_danhmuc_baiviet WHERE tbl_danhmuc_baiviet.id_baiviet='$_GET[id]' LIMIT 1";
     $query_name= mysqli_query($mysqli,$sql_name);
     $row_title= mysqli_fetch_array($query_name);
 ?>
-<h3>Danh Mục Bài viết: <span style="text-align: center; text-transform: uppercase"> <?php echo $row_title['name_bv']?> </span>  </h3>
+<h3>Danh Mục Bài viết: <span style="text-align: center; text-transform: uppercase"> <?php echo $row_title['tendanhmuc_baiviet']?> </span>  </h3>
 <ul class="product_list">
 	<?php
 		while($row_bv   = mysqli_fetch_array($query_bv)){
 	?>
 	<li style="text-decoration:none">
-		<a  href="index.php?quanly=baiViet&id=<?php echo $row_bv['id_bv']?>	" >
-		<img src="admincp/modules/quanLyBaiViet/uploads/<?php echo $row_bv['hinh_anh'] ?>">
+		<a  href="index.php?quanly=baiViet&id=<?php echo $row_bv['id']?>	" >
+		<img src="admincp/modules/quanLyBaiViet/uploads/<?php echo $row_bv['hinhanh'] ?>">
 		</a>
-		<p class="product_name"><?php echo $row_bv['ten_bv'] ?></p>
-        <p class="product_name"><?php echo $row_bv['tom_tat'] ?></p>
+		<p class="product_name"><?php echo $row_bv['tenbaiviet'] ?></p>
+        <p class="product_name"><?php echo $row_bv['tomtat'] ?></p>
 						
 	</li>
 	<?php 
