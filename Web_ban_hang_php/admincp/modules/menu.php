3e120dbf82b1f29b6d9e81bc79b53c8de573e38c
DanhCaTuanNgoc <127586212+DanhCaTuanNgoc@users.noreply.github.com>
2024-10-05 20:06:38 +0700

    Add files via upload

--- Web_ban_hang_php/admincp/modules/menu.php
+++ Web_ban_hang_php/admincp/modules/menu.php
@@ -1,12 +1,7 @@
-
 <ul class="admin_list">
 	<li><a href="index.php?action=quanLyDanhMucSanPham&query=them">Quản lý danh mục sản phẩm</a></li>
 	<li><a href="index.php?action=quanLySanPham&query=them">Quản lý sản phẩm</a></li>
-
 	<li><a href="index.php?action=quanLyDanhMucBaiViet&query=them">Quản lý danh mục bài viết</a></li>
 	<li><a href="index.php?action=quanLyBaiViet&query=them">Quản lý bài viết</a></li>
-	
-	<li><a href="index.php?action=quanLyDanhMucTinTuc&query=them">Quản lý danh mục tin tức</a></li>
-	<li><a href="index.php?action=quanLyTinTuc&query=them">Quản lý tin tức</a></li>
 	<li><a href="index.php?action=quanLyDonHang&query=lietke">Quản lý don hang</a></li>
 </ul>
