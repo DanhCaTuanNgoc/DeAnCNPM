693d4655928c6d55dae37174dec2e1ba133c7217
capijim <153265171+capijim@users.noreply.github.com>
2024-10-07 12:53:56 +0700

    Add files via upload

--- Web_ban_hang_php/admincp/modules/dashboard.php
+++ Web_ban_hang_php/admincp/modules/dashboard.php
@@ -1,1 +1,10 @@
-<h3>Wellcome To DASHBOARD</h3>
+<h3>Thống kê đơn hàng theo: <span id="text-date"></span></h3>
+<p>
+    <select class ="select_date">
+        <option value="7ngay">7 ngày qua</option>
+        <option value="28ngay">28 ngày qua</option>
+        <option value="90ngay">90 ngày qua</option>
+        <option value="365ngay">365 ngày qua</option>
+    </select>
+</p>
+<div id="chart" style="height: 250px;"></div>
