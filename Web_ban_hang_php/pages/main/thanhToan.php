00750b29143a4d04e9dd38d57826662da887ca7f
capijim <153265171+capijim@users.noreply.github.com>
2024-10-07 13:03:34 +0700

    Update thanhToan.php

--- Web_ban_hang_php/pages/main/thanhToan.php
+++ Web_ban_hang_php/pages/main/thanhToan.php
@@ -1,12 +1,14 @@
-<?php 
+<?php
   session_start();
   include('../../admincp/config/config.php');
-  require('../../mail/sendmail.php');
-    
+  require('../../Carbon-3.8.0/autoload.php');
+  use Carbon\Carbon;
+  use Carbon\CarbonInterval;
+
+  $now = Carbon::now('Asia/Ho_Chi_Minh');
     $id_khachhang = $_SESSION['id_khachhang'];
     $ma_gh = rand(0,9999);
-    $insert_cart = "INSERT INTO tbl_giohang(id_khachhang,ma_gh,trang_thai) VALUE('".$id_khachhang."','".$ma_gh."',1)";
-      $cart_query = mysqli_query($mysqli,$insert_cart);
+    $insert_cart = "INSERT INTO tbl_giohang(id_khachhang,ma_gh,trang_thai,cart_date) VALUE('".$id_khachhang."','".$ma_gh."',1,'".$now."')";
       if($cart_query){
         // add gio hang chi tiet
         foreach($_SESSION['cart'] as $key => $value){
