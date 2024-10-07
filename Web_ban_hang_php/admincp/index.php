7fd8706e9c71107160daf6f012dd1baaad6ff2ae
capijim <153265171+capijim@users.noreply.github.com>
2024-10-07 12:52:31 +0700

    Update index.php

--- Web_ban_hang_php/admincp/index.php
+++ Web_ban_hang_php/admincp/index.php
@@ -24,6 +24,63 @@
 
 		?>
 		</div> 	
+
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+		<script src="//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
+		<script src="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.min.js"></script>
+		<script type="text/javascript">
+		$(document).ready(function(){
+			thongke();
+			var char = new Morris.Line({
+			element: 'chart',
+
+			xkey: 'date',
+			
+			ykeys: ['order','sale','quantily'],
+			
+			labels: ['đơn hàng','giá','số lượng đã bán']
+		});
+
+		$('.select_date').change(function(){
+			var thoigian = $(this).val();
+			if(thoigian == '7ngay'){
+				var text = '7 ngày qua';
+			}else if(thoigian == '28ngay'){
+				var text = '28 ngày qua';
+			}else if(thoigian == '90ngay'){
+				var text = '90 ngày qua';
+			}else{
+				var text = '365 ngày qua';
+			}
+			
+			$.ajax({
+				url:"modules/thongke.php",
+				method:"POST",
+				dataType:"JSON",
+				data:{thoigian:thoigian},
+				success:function(data)
+				{
+					char.setData(data);
+					$('#text-date').text(text);
+				}
+			});
+		});
+
+			function thongke(){
+				var text='365 ngày qua';
+				$.ajax({
+					url:"modules/thongke.php",
+					method:"POST",
+					dataType:"JSON",
+
+					success:function(data){
+						char.setData(data);
+						$('#text-data').text(text);
+					}
+				});
+			}
+		});
+		</script>
 </body>
 </html>
 
