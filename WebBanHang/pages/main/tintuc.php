46cbe38612809e80eef0753121ff59581b78069b
zaikaman <91252891+zaikaman@users.noreply.github.com>
2024-10-07 22:05:34 +0700

    Update tintuc.php

--- WebBanHang/pages/main/tintuc.php
+++ WebBanHang/pages/main/tintuc.php
@@ -1,65 +1,21 @@
 <div class="main_content">
     <h3 style="text-align: center; text-transform: uppercase">Tin tức mới nhất</h3>
     <ul class="product_list">
+        <?php
+        // Fetch 10 latest news in ascending order by id
+        $sql_tintuc = "SELECT * FROM tbl_baiviet ORDER BY id ASC LIMIT 10";
+        $query_tintuc = mysqli_query($mysqli, $sql_tintuc);
+
+        while($row_tintuc = mysqli_fetch_array($query_tintuc)) {
+        ?>
         <li>
-            <a href="https://www.24h.com.vn/bong-da/boi-thuong-so-tien-khong-lo-mu-thiet-don-thiet-kep-vi-cham-sa-thai-ten-hag-c48a1608611.html">
-                <img src="admincp/modules/quanLyBaiViet/uploads/tin1.jpg" alt="MU chậm sa thải Ten Hag">
+            <a href="<?php echo $row_tintuc['link']; ?>">
+                <img src="admincp/modules/quanLyBaiViet/uploads/<?php echo $row_tintuc['hinhanh']; ?>" alt="<?php echo $row_tintuc['tenbaiviet']; ?>">
             </a>
-            <p class="product_name">MU chậm sa thải Ten Hag</p>
+            <p class="product_name"><?php echo $row_tintuc['tenbaiviet']; ?></p>
         </li>
-        <li>
-            <a href="https://www.24h.com.vn/bong-da/nong-bang-xep-hang-ngoai-hang-anh-mu-te-nhat-lich-su-chelsea-lung-lay-top-4-c48a1608578.html">
-                <img src="admincp/modules/quanLyBaiViet/uploads/tin2.jpg" alt="BXH Ngoại hạng Anh">
-            </a>
-            <p class="product_name">BXH Ngoại hạng Anh: MU tệ nhất lịch sử, Chelsea lung lay top 4</p>
-        </li>
-        <li>
-            <a href="https://www.24h.com.vn/bong-da/ten-hag-biet-cui-dau-ai-da-cuu-thay-tam-song-sot-qua-giong-bao-c48a1608663.html">
-                <img src="admincp/modules/quanLyBaiViet/uploads/tin3.jpg" alt="Ten Hag cúi đầu">
-            </a>
-            <p class="product_name">Ten Hag cúi đầu: Ai đã cứu thầy tạm sống sót?</p>
-        </li>
-        <li>
-            <a href="https://www.24h.com.vn/bong-da/lewandowski-ghi-3-ban-dinh-cao-bat-kip-haaland-dua-chiec-giay-vang-c48a1608595.html">
-                <img src="admincp/modules/quanLyBaiViet/uploads/tin4.jpg" alt="Lewandowski bắt kịp Haaland">
-            </a>
-            <p class="product_name">Lewandowski ghi 3 bàn đỉnh cao, bắt kịp Haaland đua Chiếc Giày Vàng</p>
-        </li>
-        <li>
-            <a href="https://www.24h.com.vn/bong-da/xau-ho-dan-sao-mu-thua-ong-gia-36-tuoi-rashford-bi-nghi-noi-xau-ten-hag-c48a1608619.html">
-                <img src="admincp/modules/quanLyBaiViet/uploads/tin5.jpg" alt="MU thua ông già 36 tuổi">
-            </a>
-            <p class="product_name">Xấu hổ: Dàn sao MU thua ông già 36 tuổi, Rashford bị nghi nói xấu Ten Hag</p>
-        </li>
-        <li>
-            <a href="https://www.24h.com.vn/bong-da/video-bong-da-barcelona-alaves-lewandowski-ghi-hat-trick-la-liga-c48a1608576.html">
-                <img src="admincp/modules/quanLyBaiViet/uploads/tin6.jpg" alt="Barcelona vs Alaves">
-            </a>
-            <p class="product_name">Video bóng đá: Barcelona - Alaves, Lewandowski ghi hat-trick</p>
-        </li>
-        <li>
-            <a href="https://www.24h.com.vn/bong-da/video-bong-da-brighton-tottenham-hang-cong-thang-hoa-ngoai-hang-anh-c48a1608573.html">
-                <img src="admincp/modules/quanLyBaiViet/uploads/tin7.jpg" alt="Brighton vs Tottenham">
-            </a>
-            <p class="product_name">Video bóng đá: Brighton - Tottenham, hàng công thăng hoa</p>
-        </li>
-        <li>
-            <a href="https://www.24h.com.vn/bong-da/video-futsal-brazil-argentina-dang-quang-kich-tinh-chung-ket-futsal-world-cup-c48a1608596.html">
-                <img src="admincp/modules/quanLyBaiViet/uploads/tin8.jpg" alt="Futsal World Cup">
-            </a>
-            <p class="product_name">Video futsal: Brazil - Argentina, đăng quang kịch tính chung kết Futsal World Cup</p>
-        </li>
-        <li>
-            <a href="https://www.24h.com.vn/bong-da/video-bong-da-sociedad-atletico-alvarez-ghi-ban-giay-51-la-liga-c48a1608577.html">
-                <img src="admincp/modules/quanLyBaiViet/uploads/tin9.jpg" alt="Sociedad vs Atletico">
-            </a>
-            <p class="product_name">Video bóng đá: Sociedad - Atletico, Alvarez ghi bàn giây 51</p>
-        </li>
-        <li>
-            <a href="https://www.24h.com.vn/bong-da/video-bong-da-nice-psg-chinh-thuc-mat-ngoi-dau-ligue-1-c48a1608587.html">
-                <img src="admincp/modules/quanLyBaiViet/uploads/tin10.jpg" alt="Nice vs PSG">
-            </a>
-            <p class="product_name">Video bóng đá: Nice - PSG, chính thức mất ngôi đầu Ligue 1</p>
-        </li>
+        <?php
+        }
+        ?>
     </ul>
 </div>
