4aca985bc5d37af4ebdc939c6d387c83b49df7c0
Nguyễn Đại Quốc <127086245+NguyenDaiQuoc@users.noreply.github.com>
2024-10-13 11:18:16 +0700

    Update thongtinthanhtoan.php

--- WebBanHang/pages/main/thongtinthanhtoan.php
+++ WebBanHang/pages/main/thongtinthanhtoan.php
@@ -1,9 +1,10 @@
+<div class="main_content">
 <p>Thông tin thanh toán</p>
-<div class="container">
+
     <div class="wrapper-2">
         <div class="arrow-steps clearfix">
-            <div class="step"> <span> <a href="index.php?quanly=giohang"> Giỏ hàng</a></span> </div>
-            <div class="step"> <span><a href="index.php?quanly=vanChuyen"> Vận chuyển</a></span> </div>
+            <div class="step done"> <span> <a href="index.php?quanly=giohang"> Giỏ hàng</a></span> </div>
+            <div class="step done"> <span><a href="index.php?quanly=vanChuyen"> Vận chuyển</a></span> </div>
             <div class="step current"> <span><a href="index.php?quanly=thongTinThanhToan">Thanh toán</a></span> </div>
             <div class="step "> <span><a href="index.php?quanly=donHangDaDat">Lịch sử đơn hàng</a></span> </div>
         </div>
@@ -105,7 +106,7 @@
                 </div>
                 <div class="form-check">
                     <input class="form-check-input" type="radio" name="payment" value="chuyenkhoan" id="radios2">
-                    <img style="width: 32px; height:32px" src="images/banking.jpg">
+                    <img style="width: 32px; height:32px" src="images/banking.png">
                     <label class="form-check-label" for="radios2">
                         Chuyển khoản
                     </label>
