26b0e84b29c5097d5bcb50c7084f13c6e98fded4
Nguyễn Đại Quốc <127086245+NguyenDaiQuoc@users.noreply.github.com>
2024-10-13 19:35:07 +0700

    Update camon.php

--- WebBanHang/pages/main/camon.php
+++ WebBanHang/pages/main/camon.php
@@ -5,7 +5,10 @@
         <a style="font-size: 13px; font-weight : 400; color : blue; text-decoration : underline;" href="index.php?">Quay lại trang chủ</a>
     </div>
 </div> -->
+<div class="main_content">
+<div style="width : 100%; display : flex; flex-direction : column; justify-content : center; align-items : center">
 <?php
+    
     include('admincp/config/config.php');
     require('Carbon-3.8.0/autoload.php');
     
@@ -32,31 +35,38 @@
         $cart_payment = 'momo';
         
         //insert database momo
-        $insert_momo = "INSERT INTO tbl_momo (partner_code,order_id,amount,order_info,order_type,trans_id,pay_type,code_cart) VALUE ('".$partnerCode."','".$orderId."','".$amount."','".$orderInfo."','".$orderType."','".$transId."','".$payType."','".$code_order."')";
+        $insert_momo = "INSERT INTO tbl_momo (partner_code,order_id,amount,order_info,order_type,trans_id,pay_type,code_cart) VALUES ('".$partnerCode."','".$orderId."','".$amount."','".$orderInfo."','".$orderType."','".$transId."','".$payType."','".$code_order."')";
         $cart_query = mysqli_query($mysqli,$insert_momo);
         if($cart_query){
-            $insert_cart = "INSERT INTO tbl_giohang(id_khachhang,ma_gh,trang_thai,cart_date,cart_payment,cart_shipping) VALUE('" . $id_khachhang . "','" . $ma_gh . "',1,'" . $now . "','".$cart_payment."','".$id_shipping."')";
+            $insert_cart = "INSERT INTO tbl_giohang(id_khachhang,ma_gh,trang_thai,cart_date,cart_payment,cart_shipping) VALUES('" . $id_khachhang . "','" . $ma_gh . "',1,'" . $now . "','".$cart_payment."','".$id_shipping."')";
             $cart_query = mysqli_query($mysqli, $insert_cart);
              // add gio hang chi tiet
             //insert gio hang
             foreach ($_SESSION['cart'] as $key => $value) {
                 $id_sp = $value['id'];
                 $so_luong = $value['so_luong'];
-                $insert_order_details = "INSERT INTO tbl_chitiet_gh(ma_gh,id_sp,so_luong_mua) VALUE('" . $ma_gh . "','" . $id_sp . "','" . $so_luong . "')";
+                $insert_order_details = "INSERT INTO tbl_chitiet_gh(ma_gh,id_sp,so_luong_mua) VALUES('" . $ma_gh . "','" . $id_sp . "','" . $so_luong . "')";
                 mysqli_query($mysqli, $insert_order_details);
               }              
             echo '<h3>Giao dịch thanh toán bằng MOMO thành công!</h3>';
-            echo '<p>Vui lòng vào trang <a target = "_blank" href = "#">Lịch sử đơn hàng</a> để xem chi tiết đơn hàng của bạn</p>';
+            echo '<p>Vui lòng vào trang <a target = "_blank" href = "http://localhost/DeAnCNPM-main/WebBanHang/index.php?quanly=donHangDaDat">Lịch sử đơn hàng</a> để xem chi tiết đơn hàng của bạn</p>';
         }else{
             echo'Giao dịch MOMO thất bại';
         }
+        
     }
 ?>
-
+    
+    <h4 style="margin-bottom : 5px; font-size : 18px">Cảm ơn bạn đã mua hàng, chúng tôi sẽ liên hệ trong thời gian sớm nhất</h4>
+        <img style="margin-top: 0px;" src="../images/camon.png" alt="EmtpyCart" width="200px" height="200px">
+        <a style="font-size: 13px; font-weight : 400; color : blue; text-decoration : underline;" href="index.php?">Quay lại trang chủ</a>
+</div>
+</div>
+<!-- 
 <div class="main_content">
     <div style="width : 100%; display : flex; flex-direction : column; justify-content : center; align-items : center">
         <h4 style="margin-bottom : 5px; font-size : 18px">Cảm ơn bạn đã mua hàng, chúng tôi sẽ liên hệ trong thời gian sớm nhất</h4>
         <img style="margin-top: 0px;" src="../images/camon.png" alt="EmtpyCart" width="200px" height="200px">
         <a style="font-size: 13px; font-weight : 400; color : blue; text-decoration : underline;" href="index.php?">Quay lại trang chủ</a>
     </div>
-</div>
+</div> -->
