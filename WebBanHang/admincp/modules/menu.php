e1bf0f14224d83f051d292174a07f438d7fb2fd8
zaikaman <91252891+zaikaman@users.noreply.github.com>
2024-10-14 14:29:11 +0700

    Update menu.php

--- WebBanHang/admincp/modules/menu.php
+++ WebBanHang/admincp/modules/menu.php
@@ -1,9 +1,9 @@
-<ul class="admin_list">
-	<li><a href="index.php">Thống kê</a></li>
-	<li><a href="index.php?action=quanLyDanhMucSanPham&query=them">Quản lý danh mục sản phẩm</a></li>
-	<li><a href="index.php?action=quanLySanPham&query=them">Quản lý sản phẩm</a></li>
-	<li><a href="index.php?action=quanLyDanhMucBaiViet&query=them">Quản lý danh mục bài viết</a></li>
-	<li><a href="index.php?action=quanLyBaiViet&query=them">Quản lý bài viết</a></li>
-	<li><a href="index.php?action=quanLyDonHang&query=lietke">Quản lý đơn hàng</a></li>
-	<li><a href="index.php?action=quanLyWeb&query=capnhat">Quản lý website</a></li>
+<ul class="nav flex-column">
+    <li class="nav-item"><a class="nav-link active" href="index.php">Thống kê</a></li>
+    <li class="nav-item"><a class="nav-link" href="index.php?action=quanLyDanhMucSanPham&query=them">Quản lý danh mục sản phẩm</a></li>
+    <li class="nav-item"><a class="nav-link" href="index.php?action=quanLySanPham&query=them">Quản lý sản phẩm</a></li>
+    <li class="nav-item"><a class="nav-link" href="index.php?action=quanLyDanhMucBaiViet&query=them">Quản lý danh mục bài viết</a></li>
+    <li class="nav-item"><a class="nav-link" href="index.php?action=quanLyBaiViet&query=them">Quản lý bài viết</a></li>
+    <li class="nav-item"><a class="nav-link" href="index.php?action=quanLyDonHang&query=lietke">Quản lý đơn hàng</a></li>
+    <li class="nav-item"><a class="nav-link" href="index.php?action=quanLyWeb&query=capnhat">Quản lý website</a></li>
 </ul>
