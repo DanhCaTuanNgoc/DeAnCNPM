aad202c11ac8161301d9e8ced9349fed86e081d6
zaikaman <91252891+zaikaman@users.noreply.github.com>
2024-10-14 14:28:21 +0700

    Update dashboard.php

--- WebBanHang/admincp/modules/dashboard.php
+++ WebBanHang/admincp/modules/dashboard.php
@@ -1,10 +1,10 @@
 <h3>Thống kê đơn hàng theo: <span id="text-date"></span></h3>
 <p>
-    <select class ="select_date">
+    <select class="form-select select_date" aria-label="Chọn thời gian">
         <option value="7ngay">7 ngày qua</option>
         <option value="28ngay">28 ngày qua</option>
         <option value="90ngay">90 ngày qua</option>
         <option value="365ngay">365 ngày qua</option>
     </select>
 </p>
-<div id="chart" style="height: 250px;"></div>
+<div id="chart" style="height: 250px;" class="bg-light"></div>
