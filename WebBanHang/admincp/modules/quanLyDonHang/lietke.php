d3d1765114795fa11311289bb84a572cd0d327a3
zaikaman <91252891+zaikaman@users.noreply.github.com>
2024-10-14 13:17:19 +0700

    Update lietke.php

--- WebBanHang/admincp/modules/quanLyDonHang/lietke.php
+++ WebBanHang/admincp/modules/quanLyDonHang/lietke.php
@@ -2,52 +2,64 @@
 $sql_lietke_dh = "SELECT * FROM tbl_giohang,tbl_dangky WHERE tbl_giohang.id_khachhang = tbl_dangky.id_dangky ORDER BY tbl_giohang.id_gh DESC ";
 $lietke_dh = mysqli_query($mysqli, $sql_lietke_dh);
 ?>
-<h3>Liệt Kê Don Hang</h3>
-<table style="width: 100%; border-collapse: collapse;" border="1">
-	<tr>
-		<th>Id</th>
-		<th>Ma Gio Hang</th>
-		<th>Ten Khach Hang</th>
-		<th>Dia chi</th>
-		<th>Email</th>
-		<th>SDT</th>
-		<th>Trang thai</th>
-		<th>Ngay dat</th>
-		<th>Quan ly</th>
-		<th>In Don Hang</th>
-	</tr>
-	<?php
-	$i = 0;
-	while ($row = mysqli_fetch_array($lietke_dh)) {
-		$i++;
-	?>
-		<tr>
-			<td> <?php echo $i ?></td>
-			<td> <?php echo $row['ma_gh'] ?></td>
-			<td> <?php echo $row['ten_khachhang'] ?></td>
-			<td> <?php echo $row['dia_chi'] ?></td>
-			<td> <?php echo $row['email'] ?></td>
-			<td> <?php echo $row['dien_thoai'] ?></td>
-			<td>
-				<?php
-				if ($row['trang_thai'] == 1) {
-					echo '<a href="modules/quanLyDonHang/xuLy.php?code=' . $row['ma_gh'] . '">Don hang moi</a>';
-				} else {
-					echo 'Da Xu Ly';
-				}
 
-				?>
-			</td>
-			<td><?php echo $row['cart_date'] ?></td>
-			<td>
-				<a href="index.php?action=donHang&query=xemDonHang&code=<?php echo $row['ma_gh'] ?>">Xem don hang</a>
-				<a href=""></a>
-			</td>
-			<td>
-				<a href="modules/quanLyDonHang/indonhang.php?&code=<?php echo $row['ma_gh'] ?>">In Đơn Hàng</a>
-			</td>
-		</tr>
-	<?php
-	}
-	?>
-</table>
+<!-- Link Bootstrap CSS -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+
+<div class="container mt-5">
+    <h3 class="text-center mb-4">Liệt Kê Đơn Hàng</h3>
+
+    <table class="table table-bordered table-hover text-center">
+        <thead class="table-dark">
+            <tr>
+                <th>ID</th>
+                <th>Mã Giỏ Hàng</th>
+                <th>Tên Khách Hàng</th>
+                <th>Địa Chỉ</th>
+                <th>Email</th>
+                <th>SDT</th>
+                <th>Trạng Thái</th>
+                <th>Ngày Đặt</th>
+                <th>Quản Lý</th>
+                <th>In Đơn Hàng</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            $i = 0;
+            while ($row = mysqli_fetch_array($lietke_dh)) {
+                $i++;
+            ?>
+                <tr>
+                    <td> <?php echo $i ?></td>
+                    <td> <?php echo $row['ma_gh'] ?></td>
+                    <td> <?php echo $row['ten_khachhang'] ?></td>
+                    <td> <?php echo $row['dia_chi'] ?></td>
+                    <td> <?php echo $row['email'] ?></td>
+                    <td> <?php echo $row['dien_thoai'] ?></td>
+                    <td>
+                        <?php
+                        if ($row['trang_thai'] == 1) {
+                            echo '<a href="modules/quanLyDonHang/xuLy.php?code=' . $row['ma_gh'] . '" class="btn btn-warning btn-sm">Đơn Hàng Mới</a>';
+                        } else {
+                            echo '<span class="badge bg-success">Đã Xử Lý</span>';
+                        }
+                        ?>
+                    </td>
+                    <td><?php echo $row['cart_date'] ?></td>
+                    <td>
+                        <a href="index.php?action=donHang&query=xemDonHang&code=<?php echo $row['ma_gh'] ?>" class="btn btn-info btn-sm">Xem Đơn Hàng</a>
+                    </td>
+                    <td>
+                        <a href="modules/quanLyDonHang/indonhang.php?&code=<?php echo $row['ma_gh'] ?>" class="btn btn-primary btn-sm">In Đơn Hàng</a>
+                    </td>
+                </tr>
+            <?php
+            }
+            ?>
+        </tbody>
+    </table>
+</div>
+
+<!-- Link Bootstrap JS and Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
