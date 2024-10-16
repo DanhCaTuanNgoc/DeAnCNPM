2390caf165ff23e1fc08e60f73829164003b0bc8
DanhCaTuanNgoc <127586212+DanhCaTuanNgoc@users.noreply.github.com>
2024-10-16 09:18:02 +0700

    Add files via upload

--- Web_ban_hang_php/pages/main/thanhtoan.php
+++ Web_ban_hang_php/pages/main/thanhtoan.php
@@ -6,38 +6,158 @@ require('../../Carbon-3.8.0/autoload.php');
 
 use Carbon\Carbon;
 use Carbon\CarbonInterval;
+// Kiểm tra session
+if (!isset($_SESSION['id_khachhang'])) {
+  echo "Vui lòng đăng nhập để tiếp tục.";
+  exit;
+}
 
+if (!isset($_SESSION['cart']) || empty($_SESSION['cart'])) {
+  echo "Giỏ hàng của bạn đang trống.";
+  exit;
+}
+if (!isset($_POST['payment'])) {
+  echo "payment của bạn đang trống.";
+  exit;
+}
 $now = Carbon::now('Asia/Ho_Chi_Minh');
 $id_khachhang = $_SESSION['id_khachhang'];
 $ma_gh = rand(0, 9999);
-$insert_cart = "INSERT INTO tbl_giohang(id_khachhang,ma_gh,trang_thai,cart_date) VALUE('" . $id_khachhang . "','" . $ma_gh . "',1,'" . $now . "')";
-$cart_query = mysqli_query($mysqli,$insert_cart);
+$cart_payment = $_POST['payment'];
+//lay id thong tin van chuyen
+$id_dangky = $_SESSION['id_khachhang'];
+$sql_get_vanchuyen = mysqli_query($mysqli, "SELECT * FROM tbl_giaohang WHERE id_dangky='$id_dangky' LIMIT 1");
+$row_get_vanchuyen = mysqli_fetch_array($sql_get_vanchuyen);
+$id_shipping = $row_get_vanchuyen['id_shipping'];
+$tong_tien = 0;
 
-if ($cart_query) {
+foreach ($_SESSION['cart'] as $key => $value) {
+  $thanhtien = $value['so_luong'] * $value['gia_sp'];
+  $tong_tien += $thanhtien;
+}
+if ($cart_payment == 'tienmat' || $cart_payment == 'chuyenkhoan') {
+  //insert vao don hang
+  $insert_cart = "INSERT INTO tbl_giohang(id_khachhang,ma_gh,trang_thai,cart_date,cart_payment,cart_shipping) VALUE('" . $id_khachhang . "','" . $ma_gh . "',1,'" . $now . "','" . $cart_payment . "','" . $id_shipping . "')";
+  $cart_query = mysqli_query($mysqli, $insert_cart);
   // add gio hang chi tiet
   foreach ($_SESSION['cart'] as $key => $value) {
     $id_sp = $value['id'];
     $so_luong = $value['so_luong'];
-    $thanhtien = $so_luong * $value['gia_sp'];
-    $tong_tien += $thanhtien;
     $insert_order_details = "INSERT INTO tbl_chitiet_gh(ma_gh,id_sp,so_luong_mua) VALUE('" . $ma_gh . "','" . $id_sp . "','" . $so_luong . "')";
     mysqli_query($mysqli, $insert_order_details);
   }
-  $tieude = "Đặt hàng website 7tcc.com.vn thành công!";
-  $noidung = "<p>Cảm ơn quý khách đã đặt hàng với mã đơn hàng : " . $ma_gh . " </p>
-                    <h4>Đơn đặt hàng bao gồm</h4>";
-  foreach ($_SESSION['cart'] as $key => $val) {
-    $noidung .= "<ul style = 'border: 1px solid blue; margin: 10px;'>
-                      <li><span>Tên sản phẩm: </span>" . $val['ten_sp'] . "</li>
-                      <li><span>Mã sản phẩm: </span>" . $val['ma_sp'] . "</li>
-                      <li><span>Giá: </span>" . number_format($val['gia_sp'], 0, ',', ',') . "</li>
-                      <li><span>Số lượng: </span>" . $val['so_luong'] . "</li>                     
-          </ul>";
+  header('Location:../../index.php/quanly=camon'); 
+} elseif ($cart_payment === 'vnpay') {
+  //thanh toan vnpay
+  $vnp_TxnRef = $ma_gh; //Mã đơn hàng. Trong thực tế Merchant cần insert đơn hàng vào DB và gửi mã này sang vnpay
+  $vnp_OrderInfo = 'Thanh toán đơn hàng';
+  $vnp_OrderType = 'billpayment';
+  $vnp_Amount = 10000 * 100;
+  $vnp_Locale = 'vn';
+  $vnp_BankCode = 'NCB';
+  $vnp_IpAddr = $_SERVER['REMOTE_ADDR'];
+  //Add Params of 2.0.1 Version
+  $vnp_ExpireDate = $expire;
+
+  $inputData = array(
+    "vnp_Version" => "2.1.0",
+    "vnp_TmnCode" => $vnp_TmnCode,
+    "vnp_Amount" => $vnp_Amount,
+    "vnp_Command" => "pay",
+    "vnp_CreateDate" => date('YmdHis'),
+    "vnp_CurrCode" => "VND",
+    "vnp_IpAddr" => $vnp_IpAddr,
+    "vnp_Locale" => $vnp_Locale,
+    "vnp_OrderInfo" => $vnp_OrderInfo,
+    "vnp_OrderType" => $vnp_OrderType,
+    "vnp_ReturnUrl" => $vnp_Returnurl,
+    "vnp_TxnRef" => $vnp_TxnRef,
+    "vnp_ExpireDate" => $vnp_ExpireDate
+
+  );
+
+  if (isset($vnp_BankCode) && $vnp_BankCode != "") {
+    $inputData['vnp_BankCode'] = $vnp_BankCode;
   }
-  $noidung .= "<h4>Tổng tiền đơn hàng: " . number_format($tong_tien, 0, ',', '.') . " VND</h4>";
-  $maildathang = $_SESSION['email'];
-  $mail = new Mailer();
-  $mail->dathang($tieude, $noidung, $maildathang);
-}
-unset($_SESSION['cart']);
-header('Location:../../index.php?quanly=camon');
+  // if (isset($vnp_Bill_State) && $vnp_Bill_State != "") {
+  //     $inputData['vnp_Bill_State'] = $vnp_Bill_State;
+  // }
+
+  //var_dump($inputData);
+  ksort($inputData);
+  $query = "";
+  $i = 0;
+  $hashdata = "";
+  foreach ($inputData as $key => $value) {
+    if ($i == 1) {
+      $hashdata .= '&' . urlencode($key) . "=" . urlencode($value);
+    } else {
+      $hashdata .= urlencode($key) . "=" . urlencode($value);
+      $i = 1;
+    }
+    $query .= urlencode($key) . "=" . urlencode($value) . '&';
+  }
+
+  $vnp_Url = $vnp_Url . "?" . $query;
+  if (isset($vnp_HashSecret)) {
+    $vnpSecureHash =   hash_hmac('sha512', $hashdata, $vnp_HashSecret); //  
+    $vnp_Url .= 'vnp_SecureHash=' . $vnpSecureHash;
+  }
+  $returnData = array(
+    'code' => '00',
+    'message' => 'success',
+    'data' => $vnp_Url
+  );
+  if (isset($_POST['thanhToan'])) {
+    $insert_cart = "INSERT INTO tbl_giohang(id_khachhang,ma_gh,trang_thai,cart_date) VALUE('" . $id_khachhang . "','" . $ma_gh . "',1,'" . $now . "')";
+    $cart_query = mysqli_query($mysqli, $insert_cart);
+    // add gio hang chi tiet
+    foreach ($_SESSION['cart'] as $key => $value) {
+      $id_sp = $value['id'];
+      $so_luong = $value['so_luong'];
+      $thanhtien = $so_luong * $value['gia_sp'];
+      $tong_tien += $thanhtien;
+      $insert_order_details = "INSERT INTO tbl_chitiet_gh(ma_gh,id_sp,so_luong_mua) VALUE('" . $ma_gh . "','" . $id_sp . "','" . $so_luong . "')";
+      mysqli_query($mysqli, $insert_order_details);
+    }
+    $_SESSION['code_cart'] = $ma_gh;
+    echo '!!!';
+    header('Location: ' . $vnp_Url);  
+    die();
+  } else {
+    echo json_encode($returnData);
+  }
+} 
+
+
+        // ui lòng tham khảo thêm tại code demo
+
+
+// if ($cart_query) {
+//   // add gio hang chi tiet
+//   foreach ($_SESSION['cart'] as $key => $value) {
+//     $id_sp = $value['id'];
+//     $so_luong = $value['so_luong'];
+//     $thanhtien = $so_luong * $value['gia_sp'];
+//     $tong_tien += $thanhtien;
+//     $insert_order_details = "INSERT INTO tbl_chitiet_gh(ma_gh,id_sp,so_luong_mua) VALUE('" . $ma_gh . "','" . $id_sp . "','" . $so_luong . "')";
+//     mysqli_query($mysqli, $insert_order_details);
+//   }
+//   $tieude = "Đặt hàng website 7tcc.com.vn thành công!";
+//   $noidung = "<p>Cảm ơn quý khách đã đặt hàng với mã đơn hàng : " . $ma_gh . " </p>
+//                     <h4>Đơn đặt hàng bao gồm</h4>";
+//   foreach ($_SESSION['cart'] as $key => $val) {
+//     $noidung .= "<ul style = 'border: 1px solid blue; margin: 10px;'>
+//                       <li><span>Tên sản phẩm: </span>" . $val['ten_sp'] . "</li>
+//                       <li><span>Mã sản phẩm: </span>" . $val['ma_sp'] . "</li>
+//                       <li><span>Giá: </span>" . number_format($val['gia_sp'], 0, ',', ',') . "</li>
+//                       <li><span>Số lượng: </span>" . $val['so_luong'] . "</li>                     
+//           </ul>";
+//   }
+//   $noidung .= "<h4>Tổng tiền đơn hàng: " . number_format($tong_tien, 0, ',', '.') . " VND</h4>";
+//   $maildathang = $_SESSION['email'];
+//   $mail = new Mailer();
+//   $mail->dathang($tieude, $noidung, $maildathang);
+// }
+// unset($_SESSION['cart']);
+// header('Location:../../index.php?quanly=camon')
--- Web_ban_hang_php/pages/main/thongtinthanhtoan.php
+++ Web_ban_hang_php/pages/main/thongtinthanhtoan.php
@@ -1,33 +1,33 @@
+<div class="main_content">
 <p>Thông tin thanh toán</p>
-<div class="container">	
-<div class="wrapper-2">
-    <div class="arrow-steps clearfix">
-        <div class="step"> <span> <a href="index.php?quanly=giohang"> Giỏ hàng</a></span> </div>
-        <div class="step"> <span><a href="index.php?quanly=vanChuyen"> Vận chuyển</a></span> </div>
-        <div class="step current"> <span><a href="index.php?quanly=thongTinThanhToan">Thanh toán</a></span> </div>
-        <div class="step "> <span><a href="index.php?quanly=donHangDaDat">Lịch sử đơn hàng</a></span> </div>
+
+    <div class="wrapper-2">
+        <div class="arrow-steps clearfix">
+            <div class="step done"> <span> <a href="index.php?quanly=giohang"> Giỏ hàng</a></span> </div>
+            <div class="step done"> <span><a href="index.php?quanly=vanChuyen"> Vận chuyển</a></span> </div>
+            <div class="step current"> <span><a href="index.php?quanly=thongTinThanhToan">Thanh toán</a></span> </div>
+            <div class="step "> <span><a href="index.php?quanly=donHangDaDat">Lịch sử đơn hàng</a></span> </div>
         </div>
-</div>
-<form action="pages/main/xuLyThanhToan.php" method="POST">
+    </div>
+    <form action="pages/main/thanhtoan.php" method="POST">
         <div class="row">
             <?php
-      $id_dangky = $_SESSION['id_khachhang'];
-      $sql_get_vanchuyen = mysqli_query($mysqli,"SELECT * FROM tbl_giaohang WHERE id_dangky='$id_dangky' LIMIT 1");
-      $count = mysqli_num_rows($sql_get_vanchuyen);
-      if($count > 0){
-        $row_get_vanchuyen = mysqli_fetch_array($sql_get_vanchuyen);
-        $name = $row_get_vanchuyen['name'];
-        $phone = $row_get_vanchuyen['phone'];
-        $address = $row_get_vanchuyen['address'];
-        $note = $row_get_vanchuyen['note'];
-      }
-      else{
-        $name = '';
-        $phone = '';
-        $address = '';
-        $note = '';
-      }
-      ?>
+            $id_dangky = $_SESSION['id_khachhang'];
+            $sql_get_vanchuyen = mysqli_query($mysqli, "SELECT * FROM tbl_giaohang WHERE id_dangky='$id_dangky' LIMIT 1");
+            $count = mysqli_num_rows($sql_get_vanchuyen);
+            if ($count > 0) {
+                $row_get_vanchuyen = mysqli_fetch_array($sql_get_vanchuyen);
+                $name = $row_get_vanchuyen['name'];
+                $phone = $row_get_vanchuyen['phone'];
+                $address = $row_get_vanchuyen['address'];
+                $note = $row_get_vanchuyen['note'];
+            } else {
+                $name = '';
+                $phone = '';
+                $address = '';
+                $note = '';
+            }
+            ?>
             <div class="col-md-8" style="float:left;width: 680px;">
                 <h4>THÔNG TIN VẬN CHUYỂN VÀ GIỎ HÀNG</h4>
                 <ul>
@@ -49,49 +49,49 @@
                     </tr>
 
                     <?php
-                if(isset($_SESSION['cart'])){
-                    $i=0;
-                    $tongtien=0;
-                    foreach($_SESSION['cart'] as $cart_item){
-                    $thanhtien= $cart_item['so_luong']*$cart_item['gia_sp'];
-                    $tongtien+=$thanhtien;
-                    $i++;
-                ?>
-                    <tr>
-                        <td><?php echo $i; ?></td>
-                        <td><?php echo $cart_item['ma_sp']  ?></td>
-                        <td><?php echo $cart_item['ten_sp']  ?></td>
-                        <td><img src="admincp/modules/quanLySanPham/uploads/<?php echo $cart_item['hinh_anh'];?>"
-                                width="150px"></td>
-                        <td>
-                            <!-- <a href="pages/main/themGioHang.php?cong=<?php echo $cart_item['id']?>"><i class="fa-solid fa-plus"></i></a> -->
-                            <?php echo $cart_item['so_luong'] ?>
-                            <!-- <a href="pages/main/themGioHang.php?tru=<?php echo $cart_item['id']?>"><i class="fa-solid fa-minus"></i></a>   -->
-                        </td>
-                        <td><?php echo number_format($cart_item['gia_sp'],0,',','.').' VND ' ?></td>
-                        <td><?php echo number_format($thanhtien,0,',','.').' VND ' ?></td>
-                    </tr>
-                    <?php 
-                }
-                //$_SESSION['cart']['tong_tien'] = $tongtien;
-                ?>
-                    <tr>
-                        <td colspan="8">
-                            <p style="float: left;">Tong tien : <?php echo number_format($tongtien,0,',','.').' VND ' ?>
-                            </p>
-                            <div style="clear: both;"></div>
-                        </td>
-                    </tr>
-                    <?php 
-                }else{
-                ?>
-                    <tr>
-                        <td colspan="8">
-                            <p>Hien tai khong co san pham</p>
-                        </td>
-                    </tr>
-                    <?php 
-                } ?>
+                    if (isset($_SESSION['cart'])) {
+                        $i = 0;
+                        $tongtien = 0;
+                        foreach ($_SESSION['cart'] as $cart_item) {
+                            $thanhtien = $cart_item['so_luong'] * $cart_item['gia_sp'];
+                            $tongtien += $thanhtien;
+                            $i++;
+                    ?>
+                            <tr>
+                                <td><?php echo $i; ?></td>
+                                <td><?php echo $cart_item['ma_sp']  ?></td>
+                                <td><?php echo $cart_item['ten_sp']  ?></td>
+                                <td><img src="admincp/modules/quanLySanPham/uploads/<?php echo $cart_item['hinh_anh']; ?>"
+                                        width="150px"></td>
+                                <td>
+                                    <!-- <a href="pages/main/themGioHang.php?cong=<?php echo $cart_item['id'] ?>"><i class="fa-solid fa-plus"></i></a> -->
+                                    <?php echo $cart_item['so_luong'] ?>
+                                    <!-- <a href="pages/main/themGioHang.php?tru=<?php echo $cart_item['id'] ?>"><i class="fa-solid fa-minus"></i></a>   -->
+                                </td>
+                                <td><?php echo number_format($cart_item['gia_sp'], 0, ',', '.') . ' VND ' ?></td>
+                                <td><?php echo number_format($thanhtien, 0, ',', '.') . ' VND ' ?></td>
+                            </tr>
+                        <?php
+                        }
+                        //$_SESSION['cart']['tong_tien'] = $tongtien;
+                        ?>
+                        <tr>
+                            <td colspan="8">
+                                <p style="float: left;">Tong tien : <?php echo number_format($tongtien, 0, ',', '.') . ' VND ' ?>
+                                </p>
+                                <div style="clear: both;"></div>
+                            </td>
+                        </tr>
+                    <?php
+                    } else {
+                    ?>
+                        <tr>
+                            <td colspan="8">
+                                <p>Hien tai khong co san pham</p>
+                            </td>
+                        </tr>
+                    <?php
+                    } ?>
                 </table>
 
             </div>
@@ -106,7 +106,7 @@
                 </div>
                 <div class="form-check">
                     <input class="form-check-input" type="radio" name="payment" value="chuyenkhoan" id="radios2">
-                    <img style="width: 32px; height:32px" src="images/banking.jpg">
+                    <img style="width: 32px; height:32px" src="images/banking.png">
                     <label class="form-check-label" for="radios2">
                         Chuyển khoản
                     </label>
