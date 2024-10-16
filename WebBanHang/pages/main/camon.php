f518ba4bd3e67687fefcfe864ea1e7d7dba1354d
capijim <153265171+capijim@users.noreply.github.com>
2024-10-16 08:28:06 +0700

    Update camon.php

--- WebBanHang/pages/main/camon.php
+++ WebBanHang/pages/main/camon.php
@@ -54,7 +54,28 @@
             echo'Giao dịch MOMO thất bại';
         }
         
-    }
+    }elseif(isset($_GET['vnp_Amount'])){    
+            $vnp_Amount=$_GET['vnp_Amount'];
+            $vnp_BankCode=$_GET['vnp_BankCode'];
+            $vnp_BankTranNo=$_GET['vnp_BankTranNo'];
+            $vnp_CardType=$_GET['vnp_CardType'];
+            $vnp_OrderInfo=$_GET['vnp_OrderInfo'];
+            $vnp_PayDate=$_GET['vnp_PayDate'];
+            $vnp_TmnCode=$_GET['vnp_TmnCode'];
+            $vnp_TransactionNo=$_GET['vnp_TransactionNo'];
+            $code_cart = $_SESSION['code_cart'];
+
+        }
+        $insert_vnpay = "INSERT INTO tbl_vnpay(vnp_amount, vnp_bankcode, vnp_banktranno, vnp_cardtype, vnp_orderinfo, vnp_paydate, vnp_tmncode, vnp_transactionno,code_cart) 
+                VALUE ('$vnp_Amount', '$vnp_BankCode', '$vnp_BankTranNo', '$vnp_CardType', '$vnp_OrderInfo', '$vnp_PayDate', '$vnp_TmnCode', '$vnp_TransactionNo','$code_cart')";
+        $cart_query = mysqli_query($mysqli, $insert_vnpay);
+        if ($cart_query) {
+            echo '<h3>Giao dịch thanh toán bằng VNPAY thành công</h3>';
+            echo '<p>Vui lòng vào trang cá nhân <a target="_blank" href="#">lịch sử đơn hàng</a> để xem chi tiết đơn hàng của bạn</p>';
+        } else {
+            echo 'Giao dịch thất bại';
+        }
+        
 ?>
     
     <h4 style="margin-bottom : 5px; font-size : 18px">Cảm ơn bạn đã mua hàng, chúng tôi sẽ liên hệ trong thời gian sớm nhất</h4>
