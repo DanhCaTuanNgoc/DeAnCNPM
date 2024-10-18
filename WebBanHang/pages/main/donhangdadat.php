7a29a979470f430a8b626b3f751600f27a14d3ff
Nguyễn Đại Quốc <127086245+NguyenDaiQuoc@users.noreply.github.com>
2024-10-18 08:07:42 +0700

    Update donhangdadat.php

--- WebBanHang/pages/main/donhangdadat.php
+++ WebBanHang/pages/main/donhangdadat.php
@@ -1,3 +1,4 @@
+<div class="main_content">    
 <p>Đơn hàng đã đặt</p>
 <div class="wrapper-2">
     <div class="arrow-steps clearfix">
@@ -6,4 +7,5 @@
         <div class="step done"> <span><a href="index.php?quanly=thongTinThanhToan">Thanh toán</a></span> </div>
         <div class="step current"> <span><a href="index.php?quanly=donHangDaDat">Lịch sử đơn hàng</a></span> </div>
     </div>
+</div>
 </div>
