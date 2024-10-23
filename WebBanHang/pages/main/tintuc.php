7a66f31134e3b927f43a55477c7fb1c1900afcfa
zaikaman <91252891+zaikaman@users.noreply.github.com>
2024-10-23 22:37:20 +0700

    Update tintuc.php

--- WebBanHang/pages/main/tintuc.php
+++ WebBanHang/pages/main/tintuc.php
@@ -1,24 +1,37 @@
-<div class="main_content">
-    <h3 style="text-align: center; text-transform: uppercase">Tin tức mới nhất</h3>
-    <?php
-    $sql_bv = "SELECT * FROM tbl_baiviet WHERE tinhtrang=1 ORDER BY id DESC";
-    $query_bv = mysqli_query($mysqli, $sql_bv);
+<!-- Include Bootstrap CSS in your header -->
+<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
 
-    ?>
-    <ul class="product_list">
+<div class="container my-5">
+    <h3 class="text-center text-uppercase mb-4">Tin tức mới nhất</h3>
+    <div class="row">
         <?php
-        while ($row_bv   = mysqli_fetch_array($query_bv)) {
-        ?>
-            <li>
-                <a href="index.php?quanly=baiviet&id=<?php echo $row_bv['id'] ?>	">
-                    <img src="admincp/modules/quanLyBaiViet/uploads/<?php echo $row_bv['hinhanh'] ?>">
-                </a>
-                <p class="product_name"><?php echo $row_bv['tenbaiviet'] ?></p>
-                <p class="product_name"><?php echo $row_bv['tomtat'] ?></p>
+        // Fetch 10 latest news in ascending order by id
+        $sql_tintuc = "SELECT * FROM tbl_baiviet ORDER BY id ASC LIMIT 10";
+        $query_tintuc = mysqli_query($mysqli, $sql_tintuc);
 
-            </li>
+        while($row_tintuc = mysqli_fetch_array($query_tintuc)) {
+        ?>
+        <div class="col-md-4 mb-4">
+            <div class="card h-100">
+                <a href="<?php echo $row_tintuc['link']; ?>">
+                    <img src="admincp/modules/quanLyBaiViet/uploads/<?php echo $row_tintuc['hinhanh']; ?>" class="card-img-top" alt="<?php echo $row_tintuc['tenbaiviet']; ?>">
+                </a>
+                <div class="card-body text-center">
+                    <h5 class="card-title">
+                        <a href="<?php echo $row_tintuc['link']; ?>" class="text-dark">
+                            <?php echo $row_tintuc['tenbaiviet']; ?>
+                        </a>
+                    </h5>
+                </div>
+            </div>
+        </div>
         <?php
         }
         ?>
-    </ul>
+    </div>
 </div>
+
+<!-- Include Bootstrap JS and dependencies in your footer -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
